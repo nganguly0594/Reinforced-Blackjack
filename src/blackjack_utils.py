968db5925b37484b84ffdfbb99d613bd91dc992b
nganguly0594 <neilganguly2015@gmail.com>
2024-06-29 03:04:34 -0500

    Finished game logic and interactive gamemode

---
+++ src/blackjack_utils.py
@@ -0,0 +1,188 @@
+"""
+This is the main blackjack setup module containing OOP based
+implementations of card, hand, and deck classes, as well as
+functions to define the game logic and start a simulation run
+"""
+
+import random
+
+# All cards and suits, aces are considered 11 to begin
+card_values = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "Queen": 10,
+    "Jack": 10,
+    "King": 10,
+    "Ace": 11,
+}
+
+suits = {"Club", "Spade", "Diamond", "Heart"}
+
+
+# Playing card class with each card having rank, suit, value (e.g. Jack, Diamond, 10)
+class Card:
+    def __init__(self, rank, suit, value):
+        self.rank = rank
+        self.suit = suit
+        self.value = value
+
+    def __eq__(self, other):
+        return self.value == other.value  # To check value equivalence for split
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}s"
+
+
+# Hand assumes that ace is 11 until it would cause player to bust, then it
+# simulates changing the value of ace to 1
+class Hand:
+    def __init__(self):
+        self.player_cards = []
+        self.eleven_aces = 0  # Number of aces being used with 11 value, not 1
+        self.card_total = 0
+
+    def receive_card(self, new_card):
+        if new_card.rank == "Ace":
+            self.eleven_aces += 1
+
+        self.player_cards.append(new_card)
+        self.card_total += new_card.value
+
+        if self.card_total > 21 and self.eleven_aces > 0:
+            self.card_total -= 10  # There is an ace worth 11, so the ace is now 1
+            self.eleven_aces -= 1  # Remove ace from list of aces
+
+    def value(self):
+        return self.card_total
+
+    def has_eleven_ace(self):
+        return self.eleven_aces > 0
+
+    def split(self, other):
+        other.receive_card(self.player_cards.pop())
+
+        # Managing case where aces are split vs. other cards
+        if self.player_cards[0].rank == "Ace":
+            self.card_total -= 1
+        else:
+            self.card_total -= self.player_cards[0].value
+
+    def __getitem__(self, card_number):
+        return self.player_cards[card_number]
+
+    def __len__(self):
+        return len(self.player_cards)
+
+    def __str__(self):
+        return (
+            "hand is worth "
+            + str(self.value())
+            + " and contains:\n"
+            + "\n".join([str(card) for card in self.player_cards])
+        )
+
+
+# This will be used to simulate the shoe of cards, so we can make shoes
+# by picking any arbitrary number of decks
+class Decks:
+    def __init__(self, num_decks=1):
+        # List comprehension to create one deck of cards
+        self.cards = [
+            Card(rank, suit, value)
+            for rank, value in card_values.items()
+            for suit in suits
+        ]
+
+        self.cards *= num_decks
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self, hand):
+        hand.receive_card(self.cards.pop())  # Cards will be dealt directly to hands
+
+    def __str__(self):
+        return "\n".join([str(card) for card in self.cards])
+
+    def __len__(self):
+        return len(self.cards)
+
+
+# Setting the dealer logic -
+# The dealer has predetermined rules for hitting and standing. Simulating Soft 17 rules,
+# so if dealer reaches 17 with an ace worth 11 then they have to hit again.
+def dealer_simulation(hand, deck):
+    while hand.value() < 17 or (hand.value() == 17 and hand.has_eleven_ace()):
+        deck.deal(hand)
+
+
+# Blackjack Configuration
+# - Cards reshuffled after 25% deck penetration
+# - 1:1 payout
+# - Dealer hits soft 17
+# - Player actions: (H)it, (S)tand, s(P)lit, (D)ouble down
+# - No resplits
+# - No double down after split
+# - No surrender
+
+# Customizable Configuration
+# - Player strategy
+# - Initial money (Default: 2000)
+# - Bet amount (Default: 100)
+# - Max rounds played (Default: 1)
+# - Number of decks used for the shoe (Default: 6)
+
+
+def simulation(strategy, initial_money=1000, bet_amount=100, max_rounds=1, decks=6):
+    rounds_played = 0
+    current_money = initial_money
+    penetration_limit = decks * 13  # 25% deck penetration for reshuffling
+
+    shoe = Decks(decks)
+    shoe.shuffle()
+
+    while rounds_played < max_rounds and bet_amount <= current_money:
+        rounds_played += 1
+
+        if len(shoe) <= penetration_limit:
+            shoe = Decks(decks)
+            shoe.shuffle()
+
+        player_hands = [Hand()]  # Create list of hands due to possibility of split
+        dealer_hand = Hand()
+
+        for _ in range(2):
+            shoe.deal(player_hands[0])
+            shoe.deal(dealer_hand)
+
+        dealer_upcard = dealer_hand[0]
+
+        can_double = current_money >= bet_amount  # Ensure player can double their bet
+
+        # Player's turn
+        double = strategy(
+            player_hands, shoe, dealer_upcard=dealer_upcard, can_bet_more=can_double
+        )
+
+        # Dealer's turn
+        dealer_simulation(dealer_hand, shoe)
+
+        current_bet = bet_amount * (int(double) + 1)  # Double the bet if doubled down
+
+        for i, hand in enumerate(player_hands):
+            if hand.value() > 21:
+                current_money -= current_bet
+            elif dealer_hand.value() > 21 or dealer_hand.value() < hand.value():
+                current_money += current_bet  # Payout for win
+            elif dealer_hand.value() > hand.value():
+                current_money -= current_bet
+            # Else tie, so do nothing
+
+    return current_money - initial_money
---
+++ src/main.py
@@ -0,0 +1,214 @@
+"""
+This is the interactive play module, which will allow users to simulate
+blackjack play against the dealer with customizable configurations
+"""
+
+import os
+
+from blackjack_utils import Decks, Hand, dealer_simulation
+
+
+def player_simulation(hands, dealer_upcard, deck, can_bet_more, rounds):
+    """
+    Function to allow user to simulate their turn in blackjack
+
+    Returns whether the player chose to double down or not, in
+    order to manipulate the game's bet amount
+    """
+    split = False
+    double = False
+    first_action = True  # To allow double down after the first 2 cards
+
+    done = False
+
+    while hands[0].value() < 21 and not done:
+        os.system("clear")
+        print(f"*****Round {rounds}*****")
+        print("The dealer's up card is", dealer_upcard, "\n")
+        print("Your", hands[0], "\n")
+
+        while True:
+            print("What action will you take?")
+            print("(H)it  (S)tand", end="")
+
+            if first_action and can_bet_more:
+                print("  (D)ouble down", end="")
+
+                if hands[0][0] == hands[0][1]:
+                    same_card = True  # Check if player can split
+                    print("  s(P)lit", end="")
+
+            print("\n\n")
+
+            action = input().upper()
+
+            print("\n")
+
+            if action == "H":
+                deck.deal(hands[0])
+                first_action = False
+                break
+            elif action == "S":
+                done = True
+                break
+            elif action == "D" and first_action:
+                double = True
+                deck.deal(hands[0])
+                done = True
+                break
+            elif action == "P" and same_card:
+                split = True
+                done = True
+                break
+            else:
+                print("Invalid input, try again!", "\n")
+
+    if split:
+        hands.append(Hand())  # Add a new hand for splitting
+        hands[0].split(hands[1])  # Split the cards between hands
+
+        for i, hand in enumerate(hands):
+            deck.deal(hand)
+
+            stand = False
+
+            while hand.value() < 21 and not stand:
+                os.system("clear")
+                print(f"*****Round {rounds}*****")
+                print(f"Now playing hand {i + 1}...")
+                print("The dealer's up card is a", dealer_upcard, "\n")
+                print("Your", hand, "\n")
+
+                while True:
+                    print("What action will you take?")
+                    print("(H)it  (S)tand", end="")
+
+                    print("\n\n")
+
+                    action = input().upper()
+
+                    print("\n")
+
+                    if action == "H":
+                        deck.deal(hand)
+                        break
+                    elif action == "S":
+                        stand = True
+                        break
+                    else:
+                        print("Invalid input, try again!", "\n")
+
+        os.system("clear")
+        print(f"*****Round {rounds}*****")
+        print("Your first", hands[0], "\n")
+        print("Your second", hands[1], "\n")
+
+    else:
+        os.system("clear")
+        print(f"*****Round {rounds}*****")
+        print("Your", hands[0], "\n")
+
+    return double
+
+
+def play_interactive_game():
+    """
+    Function to run the blackjack simulation in the terminal
+
+    Asks the user to declare initial parameters such as money,
+    bet amount, rounds, and decks, and clears output after every
+    round
+    """
+    os.system("clear")
+    print("Welcome to Blackjack!\n")
+    initial_money = float(input("Enter initial money: "))
+    bet_amount = float(input("Enter bet amount: "))
+    max_rounds = int(input("Enter number of rounds: "))
+    decks = int(input("Enter number of decks to use: "))
+
+    rounds_played = 0
+    current_money = initial_money
+    penetration_limit = decks * 13  # 25% deck penetration for reshuffling
+
+    shoe = Decks(decks)
+    shoe.shuffle()
+
+    while rounds_played < max_rounds and bet_amount <= current_money:
+        rounds_played += 1
+        os.system("clear")  # Clear the terminal for a clean round start
+
+        if len(shoe) <= penetration_limit:
+            shoe = Decks(decks)
+            shoe.shuffle()
+
+        player_hands = [Hand()]
+        dealer_hand = Hand()
+
+        for _ in range(2):
+            shoe.deal(player_hands[0])
+            shoe.deal(dealer_hand)
+
+        dealer_upcard = dealer_hand[0]
+
+        can_double = current_money >= bet_amount  # Ensure player can double their bet
+
+        # Player's turn
+        double = player_simulation(
+            player_hands, dealer_upcard, shoe, can_double, rounds_played
+        )
+
+        # Dealer's turn
+        dealer_simulation(dealer_hand, shoe)
+
+        current_bet = bet_amount * (int(double) + 1)  # Double the bet if doubled down
+
+        print("Dealer's turn complete!")
+        print("The dealer's", dealer_hand, "\n")
+
+        for i, hand in enumerate(player_hands):
+            if len(player_hands) == 2:
+                print(f"Hand {i}:")
+
+            if hand.value() > 21:
+                print("Bust!", "\n")
+                current_money -= current_bet
+            elif dealer_hand.value() > 21 or dealer_hand.value() < hand.value():
+                print("Win!", "\n")
+                current_money += current_bet  # Payout for win
+            elif dealer_hand.value() > hand.value():
+                print("Lose!", "\n")
+                current_money -= current_bet
+            else:
+                print("Tie!", "\n")
+
+        if rounds_played != max_rounds:
+            print(f"Money left: ${current_money:.2f}")
+            print(f"Net gain/loss: ${(current_money - initial_money):.2f}", "\n")
+            print("Press enter to continue to next round...")
+            input()
+        else:
+            print("Press enter to cash out...")
+            input()
+
+    os.system("clear")
+
+    final_amount = current_money - initial_money
+
+    print(f"Final money left: ${current_money:.2f}")
+    print(f"Final gain/loss: ${final_amount:.2f}\n")
+
+    if rounds_played < max_rounds:
+        print("Ouch, you lost your money before finishing all the rounds :(\n")
+
+    if final_amount > 0:
+        print("You're great at this, try your odds at a real casino!")
+    elif final_amount < 0:
+        print("Don't lose hope, you're one bet away from your next big win!")
+    else:
+        print("You barely scraped by, try harder next time!")
+
+    print("\n\n")
+
+
+if __name__ == "__main__":
+    play_interactive_game()
